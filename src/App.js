6b9c987493b2ef7cb33203c5765fe5050a330926
Emmanuel Ugwu <>
2022-04-19 15:27:49 +0100

    New changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import styled from "styled-components";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Homepage,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      Furniture Store
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products:id" children={<SingleProduct />} />
+      </Routes>
+    </Router>
   );
 }
 
