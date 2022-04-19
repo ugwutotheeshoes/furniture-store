6f4e95a934357957cd65c065019c9a81b9b1c0c8
Emmanuel Ugwu <>
2022-04-19 15:28:00 +0100

    New Changes

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import Cart from "./Cart";
+import Checkout from "./Checkout";
+import Error from "./Error";
+import Homepage from "./Homepage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+
+export {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Homepage,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
