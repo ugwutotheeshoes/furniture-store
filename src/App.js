d9014a508e8c758e50b770f4395a7a70934a01e8
Emmanuel Ugwu <>
2022-04-20 10:12:10 +0100

    Made Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
+import { 
+  Footer, 
+  Navbar, 
+  Sidebar 
+} from "./components";
+
 import {
   About,
   Cart,
@@ -18,17 +20,22 @@ import {
 
 function App() {
   return (
-    <Router>
+    // <Router>
+    <div className="App">
+      <Navbar />
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="*" element={<Error />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products:id" children={<SingleProduct />} />
+        <Route path="/products" element={<Products />} >
+        <Route path="/products:id" element={<SingleProduct />} />
+          </Route>
       </Routes>
-    </Router>
+      <Footer />
+      </div>
   );
 }
 
