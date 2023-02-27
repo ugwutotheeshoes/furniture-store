a9c5215d3492829ff5fcc3991a864cfc68527959
Emmanuel Ugwu <>
2023-02-27 22:13:22 +0100

    Update FeaturedProducts.js

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -24,7 +24,7 @@ const FeaturedProducts = () => {
         <h2>featured products</h2>
         <div className="underline"></div>
         <div className="section-center featured">
-          {featured.map((product) => {
+          {featured.slice(0, 3).map((product) => {
             return <Product key={product.id} {...product} />;
           })}
         </div>
