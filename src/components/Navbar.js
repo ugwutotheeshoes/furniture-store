85d4d84db615c470daa0a4743391870fcb4ffe4d
Emmanuel Ugwu <>
2023-03-23 22:15:55 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -147,7 +147,7 @@ const NavContainer = styled.nav`
 
   @media (max-width: 800px) {
     .auth-btn {
-      margin-bottom: 2px;
+      margin-bottom: 4px;
       svg {
         font-size: 1.8rem;
       }
